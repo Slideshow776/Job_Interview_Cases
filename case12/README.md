676d41f29bc82c9955eb12511e033b8ca9223fc2
Sandra Moen <sandramoen01@gmail.com>
2021-10-02 11:25:35 +0200

    added case 12

---
+++ case12/README.md
@@ -0,0 +1,6 @@
+*Find the longest word in `words` whose letters are in alphabetic order.*
+
+For example:
+
+* "for" is an alpha word -- "f" is before "o" is before "r"
+* "while" is *not* an alpha word -- "w" is after "h"
---
+++ case12/test.js
@@ -0,0 +1,25 @@
+/**
+ * Find the longest word in `words` whose letters are in alphabetic order.
+ *
+ * For example:
+ *  "for" is an alpha word -- "f" is before "o" is before "r"
+ *  "while" is *not* an alpha word -- "w" is after "h"
+ */
+
+ function findLongestAlphaWord(words) {
+    console.log("Words: ", words);
+    
+    var longestWord = words[0];
+    var alphabeticOrder = true;
+    words.forEach(element => {
+        for (let i = 0; i < element.length; i++) {
+            if (element.charAt(i) < element.charAt(i + 1))
+                alphabeticOrder = false;
+        }
+        if (element.length > longestWord.length && alphabeticOrder)
+            longestWord = element;
+    });
+    
+    console.log("longestWord: ", longestWord);
+    return longestWord;
+}
